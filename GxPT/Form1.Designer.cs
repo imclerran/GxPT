f1082fed00299ffac792e49bafdee2f91032a4f1
Ian McLerran <imclerran@protonmail.com>
2025-08-12 14:24:04 -0500

    Resize the main form

--- GxPT/Form1.Designer.cs
+++ GxPT/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 666);
+            this.ClientSize = new System.Drawing.Size(742, 866);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GxPT";
             this.ResumeLayout(false);
 
         }
