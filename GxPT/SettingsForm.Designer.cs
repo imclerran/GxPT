0888918f9cfd6ceedfc07d593f4b45e3fb9dba41
Ian McLerran <imclerran@protonmail.com>
2025-08-19 23:47:37 -0500

    Fix txtModels in SettingsForm not accepting return.

--- GxPT/SettingsForm.Designer.cs
+++ GxPT/SettingsForm.Designer.cs
@@ -200,6 +200,7 @@
             // 
             // txtModels
             // 
+            this.txtModels.AcceptsReturn = true;
             this.txtModels.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
